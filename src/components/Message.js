389d8f0c1f32c2ac7735575e44736dbc9cffd3ff
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-11 11:14:18 -0400

    Correct class name messages to message in Message Component

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Message = (props) => {
   return (
-    <li className={`messages ${props.message.type}`}>
+    <li className={`message ${props.message.type}`}>
       <div className="icon">{props.message.name.split("")[0]}</div>
       <span className="content">{props.message.content}</span>
     </li>
